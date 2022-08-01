8abf1852e21b18c74afae0dde8d741096f20c7dc
Nam Nguyen <namy.ngu@gmail.com>
2022-08-01 23:46:41 +1000

    Contact Model View and Controllers

--- MonashApp/Controllers/HomeController.cs
+++ MonashApp/Controllers/HomeController.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using MonashApp.Models;
+
 
 namespace MonashApp.Controllers
 {
@@ -21,11 +26,34 @@ namespace MonashApp.Controllers
             return View();
         }
 
+        //GET: Home/Contact
         public ActionResult Contact()
         {
             ViewBag.Message = "Contact Us Here!";
 
             return View();
         }
+        
+        //POST: Home/Contact
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(ContactViewModel model)
+        {
+            async Task SendMail(string name, string fromEmail, string message)
+            {
+                var apiKey = "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";
+                var client = new SendGridClient(apiKey);
+                var from = new EmailAddress(fromEmail, name);
+                var subject = "Sending with Twilio SendGrid is Fun";
+                var to = new EmailAddress("namy.ngu@gmail.com", "Nam's inbox");
+                var plainTextContent = message;
+                var htmlContent = message;
+                Console.WriteLine("Sending email...");
+                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var response = await client.SendEmailAsync(msg);
+            }
+        }
+            
     }
 }
