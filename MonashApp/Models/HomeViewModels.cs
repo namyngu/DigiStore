4047350a4da95b1ef87e2e59e4dbf057cbc3d4a0
Hoang Nam Nguyen <hngu0131@student.monash.edu>
2022-08-01 23:46:41 +1000

    Contact Model View and Controllers

---
+++ MonashApp/Models/HomeViewModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MonashApp.Models
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Your Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(500, ErrorMessage = "Message must be between 10-500 characters!", MinimumLength = 10)]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+    }
+}
