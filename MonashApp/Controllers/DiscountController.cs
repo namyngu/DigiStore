e78ac148a7751c9e4995fb7154bea736540b5e0e
Nam Nguyen <namy.ngu@gmail.com>
2022-08-11 23:43:46 +1000

    Added DbContext controllers and views for CustomerReview, Brand, OrderDetail, Order and Discount

---
+++ MonashApp/Controllers/DiscountController.cs
@@ -0,0 +1,92 @@
+﻿using MonashApp.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonashApp.Controllers
+{
+    public class DiscountController : Controller
+    {
+        DiscountContext db = new DiscountContext();
+
+        // GET: Discount
+        public ActionResult Index()
+        {
+            return View(db.Discounts.ToList());
+        }
+
+        // GET: Discount/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Discount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Discount/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Discount/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Discount/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Discount/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Discount/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MonashApp/Controllers/OrderDetailController.cs
@@ -0,0 +1,92 @@
+﻿using MonashApp.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonashApp.Controllers
+{
+    public class OrderDetailController : Controller
+    {
+        OrderDetailContext db = new OrderDetailContext();
+
+        // GET: OrderDetail
+        public ActionResult Index()
+        {
+            return View(db.OrderDetails.ToList());
+        }
+
+        // GET: OrderDetail/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OrderDetail/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderDetail/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrderDetail/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OrderDetail/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OrderDetail/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OrderDetail/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
