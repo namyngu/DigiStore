122e070a1428f3449d9dc8c22742621fa20d08fb
Hoang Nam Nguyen <hngu0131@student.monash.edu>
2022-08-15 03:06:51 +1000

    styling and added filter for categories

--- MonashApp/Startup.cs
+++ MonashApp/Startup.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.Owin;
+using MonashApp.Models;
 using Owin;
+using System.Collections.Generic;
 
 [assembly: OwinStartupAttribute(typeof(MonashApp.Startup))]
 namespace MonashApp
 {
     public partial class Startup
     {
+        //create a global attribute for shopping cart at the beginning.
+        public static List<Product> ShoppingCart { get; set; }
+
+        public Startup()
+        {
+            ShoppingCart = new List<Product>();
+        }
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
         }
+
+
     }
 }
