0204c992c12dd7257bd1ea6eeca4af12a058d8fc
Nam Nguyen <namy.ngu@gmail.com>
2022-08-15 21:44:10 +1000

    Added order functionality

--- MonashApp/Controllers/OrderController.cs
+++ MonashApp/Controllers/OrderController.cs
@@ -21,35 +21,70 @@ namespace MonashApp.Controllers
             return View(db.Orders.Where(m => m.AspNetUserId == id).ToList());
         }
 
+        //POST: Grab order Id then redirect to order details page
+        [HttpPost]
+        public ActionResult Index(int orderId)
+        {
+
+            return View("Details", orderId);
+        }
+
         // GET: Order/Details/5
         //Display items for that order.
+
         public ActionResult Details(int orderId)
         {
             return View(db.OrderDetails.Where(m => m.OrderId == orderId).ToList());
         }
 
         // GET: Order/Create
-        //Shows shopping cart list - same concept
+        [AllowAnonymous]
         public ActionResult Create()
         {
             //pass user ID into view if user is logged in.
             ViewBag.UserId = User.Identity.GetUserId();
+            String date = DateTime.Now.ToString("yyyy/MM/dd");
+            ViewBag.Date = date;
             return View();
         }
 
         // POST: Order/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Order newOrder)
         {
             try
             {
-                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    Order order = newOrder;
 
-                return RedirectToAction("Index");
+                    //Create an OrderDetail for each product
+                    foreach (Product item in Startup.ShoppingCart)
+                    {
+                        OrderDetail orderDetail = new OrderDetail();
+                        orderDetail.ListPrice = item.BasePrice;
+                        orderDetail.Qty = 1;
+                        orderDetail.OrderId = order.Id;
+                        orderDetail.ProductId = item.Id;
+                        
+                        //add orderDetail to order
+                        order.OrderDetails.Add(orderDetail);
+                        db.OrderDetails.Add(orderDetail);
+                    }
+
+
+                    db.Orders.Add(order);
+                    db.SaveChanges();
+
+                    Startup.ShoppingCart.Clear();
+                }
+                return RedirectToAction("Index", "Home");
             }
             catch
             {
-                return View();
+                return View("Index", "Home");
             }
         }
 
